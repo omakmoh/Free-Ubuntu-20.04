ace6ee79c3b524062626a9510074b27cdcf21bd5
SadMonday <nguyenkhacbao.dn23@gmail.com>
2021-07-04 23:36:37 +0700

    add gnome gui

--- config/setting.sh
+++ config/setting.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install xfce4 xfce4-terminal
+sudo apt install gnome-session gdm3
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg -i chrome*
 sudo apt-get install -f
