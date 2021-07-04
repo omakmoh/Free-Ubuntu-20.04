b6407f57e97405932b300eb757682257c868e192
SadMonday <nguyenkhacbao.dn23@gmail.com>
2021-07-04 23:54:50 +0700

    back to the start

--- config/setting.sh
+++ config/setting.sh
@@ -1,6 +1,5 @@
 sudo apt-get update
-sudo add-apt-repository ppa:gnome3-team/gnome3 -y
-sudo apt-get update && sudo apt-get install gnome-shell ubuntu-gnome-desktop -y
+sudo apt-get install xfce4 xfce4-terminal
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg -i chrome*
 sudo apt-get install -f
