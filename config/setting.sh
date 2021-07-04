fee9ed4e26eda943114b3a53e7d53981dbf2fa30
SadMonday <nguyenkhacbao.dn23@gmail.com>
2021-07-04 23:51:18 +0700

    fix gnome gui

--- config/setting.sh
+++ config/setting.sh
@@ -1,5 +1,6 @@
 sudo apt-get update
-sudo apt install gnome-session gdm3
+sudo add-apt-repository ppa:gnome3-team/gnome3 -y
+sudo apt-get update && sudo apt-get install gnome-shell ubuntu-gnome-desktop -y
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg -i chrome*
 sudo apt-get install -f
